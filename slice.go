3628d69e36f74844a27810c0af121a65e21b126c
xiaowang <xiaowang@qianxin.com>
2021-12-01 20:05:41 +0800

    笔记

--- slice.go
+++ slice.go
@@ -4,11 +4,18 @@ import "fmt"
 
 func main() {
 	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
-	number2 := numbers[1:3]
+	number2 := numbers[1:6]
 	number3 := numbers[:3]
 	number4 := numbers[3:]
 
 	fmt.Println("number2 == ", number2)
 	fmt.Println("number3 == ", number3)
 	fmt.Println("number4 == ", number4)
+	append(number2, 5)
+	fmt.Println("number2 == ", number2)
+	fmt.Println("c == ", c)
+
+	copy(number2, number3)
+	fmt.Println("number2 == ", number2)
+
 }
--- string.go
+++ string.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	var str string = "yyyyy"
+	str2 := "yyyyyaaa"
+	fmt.Println("str= ", str)
+	fmt.Println("str2= ", str2)
 	fmt.Println("Hello " + "World!")
 }
