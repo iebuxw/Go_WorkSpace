c0c782bd9fcbb499036fcf28d2d4a66fa71ffc14
xiaowang <xiaowang@qianxin.com>
2022-07-01 16:00:45 +0800

    管道笔记、flag包

--- 10channel.go
+++ 10channel.go
@@ -8,6 +8,8 @@ import (
 //定义
 //var 变量名 chan 数据类型
 //管道的本质是一个队列
+//当一些数据被写入通道，对应的协程将阻塞直到有其他的协程可以从此通道接收数据。
+//通道操作会通知调度器去调度其他的协程，这就是为什么程序不会一直阻塞在一个协程。
 
 //var intB chan int
 var intB = make(chan int, 50)
@@ -45,7 +47,6 @@ func writeData(intChan chan int) {
 		// 放入数据
 		intChan <- i
 		fmt.Println("writeData ", i)
-		// time.Sleep(time.Second)
 	}
 	close(intChan) // 关闭，关闭之后只能读不能写
 }
@@ -57,7 +58,9 @@ func writeData(intChan chan int) {
 //	缓冲区为空时进行读数据
 //	缓冲区满时进行写数据
 func readData(intChan chan int, exitChan chan bool) {
+	//以下差不多是固定写法，1、读取通道 2、检测是否关闭
 	for {
+		//在此，如果ok的值为true，则表示通道已打开，因此可以执行读取操作。并且，如果的值为false，则表示该通道已关闭，因此将不执行读取操作。
 		v, ok := <-intChan //从管道获取值。1、会检查是否有在写入，如果有就不会进break 2、会阻塞
 		if !ok {
 			break
@@ -65,7 +68,4 @@ func readData(intChan chan int, exitChan chan bool) {
 		//time.Sleep(time.Second)
 		fmt.Printf("readData 读到数据=%v\n", v)
 	}
-	// readData 读取完数据后，即任务完成
-	exitChan <- true
-	close(exitChan)
 }
