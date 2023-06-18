ea30ead2fbe5f47918885315243591f3f664d364
xiaowang <xiaowang@qianxin.com>
2023-06-18 23:17:59 +0800

    1

--- 0_0php2go.go
+++ 0_0php2go.go
@@ -11,4 +11,6 @@ func main() {
 	slice1 := []string{"sss", "nnnn"}
 	string2 := php2go.Implode(",", slice1)
 	fmt.Println("php的Implode函数结果：", string2)
+	ret := php2go.InArray("sss", slice1)
+	fmt.Println("php的In_array函数结果：", ret)
 }
