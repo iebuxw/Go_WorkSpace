c7ba82265965a2f3ce41da3aed4d363af7a518a2
xiaowang <xiaowang@qianxin.com>
2024-02-02 15:38:31 +0800

    管道笔记

--- 10channel.go
+++ 10channel.go
@@ -2,22 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync"
-	"time"
 )
 
 //通道（channel）是用来传递数据的一个数据结构。管道的本质是一个队列
 //作用：协程之间通信
+//通道缓冲区：相当于队列元素个数
 //var 变量名 chan 数据类型
 /*
-知识点：
-	1、chan的缓冲与无缓冲，无缓冲我把他认为是只有一个缓冲
-    2、chan的关闭，关闭后只能读不能写
-    3、chan的range迭代，阻塞迭代直到关闭
-    4、单向队列，收和发分开
-    5、chan与select，作用是可以监听多个chan
-    6、阻塞的条件：1、未关闭写写满了；2、未关闭读完了
 
 应用场景：
 	1、超时控制
@@ -25,9 +16,6 @@ import (
 	3、生产消费
 	4、控制并发数
 */
-
-//var intB chan int
-var intB = make(chan int, 50)
 
 func main() {
 	//创建管道
@@ -42,227 +30,5 @@ func main() {
 
 	//关闭管道，关闭后只能读不能写
 	close(intC)
-
-	//测试close和不需要sleep
-	testData()
-
-	//测试只读和只写的管道
-	intE := make(chan int) //   chan   //读写
-
-	go counter(intE) //生产者
-	printer(intE)    //消费者
-
-	fmt.Println("done")
-
-	//测试select
-	dofibonacci()
-
-	//测试超时控制
-	testLongTask()
-
-	//测试生产消费
-	testProducer()
-}
-
-func readData(intChan chan int) {
-	//固定写法，读取管道并检测是否关闭
-	for {
-		v, ok := <-intChan
-		if !ok {
-			break
-		}
-		fmt.Printf("readData 读到数据=%v\n", v)
-	}
-}
-
-//测试管道的关闭
-func testData() {
-	c := make(chan int)
-
-	go func() {
-		for i := 0; i < 5; i++ {
-			c <- i //这里会阻塞的写
-		}
-		//可以用defer
-		close(c) //为什么不加close会死锁不知道
-	}()
-
-	//可以用for...range简写，下面有例子
-	for {
-		//ok为true说明channel没有关闭，为false说明管道已经关闭
-		if data, ok := <-c; ok { //这里会阻塞的读
-			fmt.Println(data)
-		} else {
-			fmt.Println("chan is closed")
-			break
-		}
-	}
-
-	fmt.Println("Finished")
-}
-
-//可以将 channel 隐式转换为单向队列，只收或只发，不能将单向 channel 转换为普通 channel
-//   chan<- //只写
-func counter(out chan<- int) {
-	defer close(out)
-	for i := 0; i < 5; i++ {
-		out <- i //如果对方不读 会阻塞
-	}
-}
-
-//   <-chan //只读
-func printer(in <-chan int) {
-	//for …… range语句可以处理Channel。阻塞迭代
-	for num := range in { //range in产生的迭代值为Channel中发送的值，它会一直迭代直到channel被关闭
-		fmt.Println("这里消费：", num)
-	}
-}
-
-//测试select，select可以完成监控多个管道
-func fibonacci(c, quit chan int) {
-	x, y := 1, 1
-	for {
-		select {
-		case c <- x: //意思是如果可写
-			x, y = y, x+y
-		case <-quit: //意思是如果可读
-			fmt.Println("quit")
-			return
-		}
-	}
-}
-
-func dofibonacci() {
-	c := make(chan int)
-	quit := make(chan int)
-
-	go func() {
-		for i := 0; i < 6; i++ {
-			fmt.Println(<-c)
-		}
-		quit <- 0
-	}()
-
-	fibonacci(c, quit)
-}
-
-// 任务定时
-func longTask(ch chan bool) {
-	// 模拟需要执行的耗时任务
-	time.Sleep(5 * time.Second)
-	ch <- true
-}
-
-func testLongTask() {
-	ch := make(chan bool)
-
-	go longTask(ch)
-
-	// 使用 select 语句等待任务完成或超时
-	select {
-	case <-ch:
-		fmt.Println("任务已完成！")
-	case <-time.After(3 * time.Second):
-		fmt.Println("任务已超时！")
-	}
-}
-
-// 每隔 1 秒种，执行一次定时任务
-func timeOut2(){
-	ticker := time.Tick(1 * time.Second)
-	for {
-		select {
-			case <- ticker:
-				// 执行定时任务
-				fmt.Println("执行 1s 定时任务")
-		}
-	}
-}
-
-// 控制并发数，开启5个并发
-func aa(){
-	ch := make(chan int, 5)
-	urls := []string{"11111", "222"}
-	for _, url := range urls {
-		go func() {
-			ch <- 1
-			// 做一些事情
-			// 注意，事情里需要定义下defer close(ch)，否则发生panic就一直阻塞了
-			fmt.Println(url)
-			<- ch
-		}()
-	}
-}
-
-// 测试生产消费
-func testProducer() {
-	fmt.Println("testProducer start")
-	ch := make(chan int)            // 生产的数据放ch
-	done := make(chan bool)         // 完成标志
-
-	go producer(ch)
-	go consumer(ch, done)
-
-	<-done
-	fmt.Println("testProducer end")
-}
-
-//生产者
-func producer(ch chan<- int) {
-	rand.Seed(time.Now().Unix())  // 不加这个每次执行都返回相同的结果
-	for {
-		num := rand.Intn(100)
-		fmt.Println("producer:", num)
-		ch <- num
-	}
-}
-
-//消费者
-func consumer(ch <-chan int, done chan<- bool) {
-	var sum int
-	for i := 0; i < 10; i++ {
-		num := <-ch
-		sum += num
-		fmt.Println("Consumed:", num)
-	}
-	fmt.Println("Total sum is", sum)
-	done <- true
-}
-
-// 测试WaitGroup，作用是防止协程提前退出
-func testWaitGroup() {
-	var waitGroup sync.WaitGroup
-
-	start := time.Now()
-	waitGroup.Add(5)
-	for i := 0; i < 5; i++ {
-		go func() {
-			defer waitGroup.Done()
-			time.Sleep(time.Second)
-			fmt.Println("done")
-		}()
-	}
-
-	waitGroup.Wait()
-	fmt.Println(time.Now().Sub(start).Seconds())
-}
-
-// 阻塞函数
-func testBlock()  {
-	bufChan := make(chan int)
-	go func() {
-		for{
-			bufChan <-1
-			time.Sleep(time.Second)
-		}
-	}()
-
-	go func() {
-		for{
-			fmt.Println(<-bufChan)
-		}
-	}()
-	select{}    // 这里要注意上面一定要有一直活动的goroutine
-	//<-bufChan         --这样也行好像
 }
 
