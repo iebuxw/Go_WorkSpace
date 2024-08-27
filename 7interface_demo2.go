9e8a869bb00cb75b1060e06e8fe238d945884114
xiaowang <xiaowang@qianxin.com>
2024-08-27 15:33:02 +0800

    接口

--- 7interface_demo2.go
+++ 7interface_demo2.go
@@ -22,6 +22,9 @@ func main() {
 	// var g Goods = b1 // 这会导致编译错误
 
 	// 正确的做法是使用指针，因为最终实现Goods接口的是结构体指针
+	// Goods类型的变量等于一个指针，感觉很怪，先这样，以后再理解
 	var g Goods = &b1 // 这不会导致编译错误
+	g1 := &b1         // 这样也行
 	fmt.Println(g.GetPrice()) // 输出: 10
+	fmt.Println(g1.GetPrice()) // 输出: 10
 }
